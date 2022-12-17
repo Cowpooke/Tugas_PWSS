bec0e838b532c01ac99cf9a69c7617145ac74840
Cowpooke <118261986+Cowpooke@users.noreply.github.com>
2022-12-17 11:12:28 +0700

    Add files via upload

--- save_employee_data.php
+++ save_employee_data.php
@@ -9,9 +9,7 @@
     $check = mysqli_fetch_array(mysqli_query($con,"select * from employee where employee_id = $employee_id"));
     if ($check[0]>0){
         $msg = "id is already exists";
-
-    } elseif($input == "UPDATE") {
-         
+    } elseif($input == "UPDATE") {        
         $sql = "update employee set
             name = '$employee_name',
             address = '$employee_address',
@@ -19,65 +17,61 @@
             where employee_id = '$employee_id' ";
         $result = mysqli_query($con,$sql);
     } elseif ($input == "SUBMIT") {
-
         $sql = "insert into employee 
             (employee_id, name, address, roles)
             values
             ('$employee_id','$employee_name','$employee_address','$employee_roles')";
         $result = mysqli_query($con,$sql);
-    }
-
-    
+    }    
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <link rel="stylesheet" href="style/styles.css">
-</head>
-<body>
-    <div class="wrap">
-        <div class="box">
-                    <div class='header'>
-                        <?php
-                            if($check[0]>0){
-                                echo "<h2>".$msg."</h2>";
-                            } else {
-                                echo "<h2>Data Saved</h2>";
-                            }
-                        ?>
-                    </div>
-            <?php
-                if ($input == "update") {
-                    echo "
-                    <a href='employee_list.php'>
-                        <div class='button'>
-                            BACK TO LIST
+    <head>
+        <link rel="stylesheet" href="style/styles.css">
+    </head>
+    <body>
+        <div class="wrap">
+            <div class="box">
+                        <div class='header'>
+                            <?php
+                                if($check[0]>0){
+                                    echo "<h2>".$msg."</h2>";
+                                } else {
+                                    echo "<h2>Data Saved</h2>";
+                                }
+                            ?>
                         </div>
-                    </a>
-                    ";
-                } else {
-                    echo "
-                        <a href='dashboard.php'>
-                            <div class='button'>
-                                BACK TO HOME
-                            </div>
-                        </a>
+                <?php
+                    if ($input == "update") {
+                        echo "
                         <a href='employee_list.php'>
                             <div class='button'>
-                                SHOW LIST
+                                BACK TO LIST
                             </div>
                         </a>
-                        <a href='input_Employee.php'>
-                            <div class='button'>
-                                INPUT AGAIN
-                            </div>
-                        </a>
-                    ";
-                }
-            ?>
+                        ";
+                    } else {
+                        echo "
+                            <a href='dashboard.php'>
+                                <div class='button'>
+                                    BACK TO HOME
+                                </div>
+                            </a>
+                            <a href='employee_list.php'>
+                                <div class='button'>
+                                    SHOW LIST
+                                </div>
+                            </a>
+                            <a href='input_Employee.php'>
+                                <div class='button'>
+                                    INPUT AGAIN
+                                </div>
+                            </a>
+                        ";
+                    }
+                ?>
+            </div>
         </div>
-    </div>
-</body>
+    </body>
 </html>
 
---
+++ show_stock.php
@@ -0,0 +1,63 @@
+<?php
+    session_start();
+    if (!isset($_SESSION['username'])) {
+        header("Location: index.php");
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Stock</title>
+        <link rel="stylesheet" href="style/styles.css">
+    </head>
+    <body>
+        <div class="box">
+            <?php
+                include "connection.php";
+                $sql = "SELECT drug.drug_id,drug.name,date_modified,stock
+                                FROM drug JOIN stock ON
+                                drug.drug_id = stock.drug_id
+                                ";
+                $result = mysqli_query($con, $sql);
+            ?>
+            <table>
+                <thead>
+                    <tr>
+                        <th> </th>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Stock</th>
+                        <th>Date Modified</th>
+                        <th>option</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                        $num = 1;
+                        while ($td = mysqli_fetch_assoc($result)) {
+                            echo "      <tr>
+                                                <td>{$num}</td>
+                                                <td>{$td['drug_id']}</td>
+                                                <td>{$td['name']}</td>
+                                                <td>{$td['stock']}</td>
+                                                <td>{$td['date_modified']}</td>
+                                                <td>";
+                            if ($_SESSION['username'] == "admin") {
+                                echo "<a class='link-black' href='edit_stock.php?id={$td['drug_id']}'>Edit</a>";
+                            } if ($_SESSION['username'] == "pharmacist") {
+                                echo "<a class='link-black' href='edit_stock.php?id={$td['drug_id']}'>Edit</a>";
+                            } if ($_SESSION['username'] == "assistant") {
+                            }
+                            echo            "</td>
+                                            </tr>";
+                            $num++;
+                        }
+                    ?>
+                </tbody>
+            </table>
+            <div>
+                <a class="button" href="dashboard.php">back</a>
+            </div>
+        </div>
+    </body>
+</html>
